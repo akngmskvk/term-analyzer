76b9670bb084241b27b2d46ec5ae3d201a49f0e9
Akın Gümüşkavak <akingumuskavak@gmail.com>
2020-06-07 04:59:49 +0300

    Add relations, fix Neo4j setup, edit code style

--- term-analyzer.py
+++ term-analyzer.py
@@ -2,6 +2,7 @@ import spacy, requests
 from SPARQLWrapper import SPARQLWrapper, JSON
 from bs4 import BeautifulSoup
 from tabulate import tabulate
+import pickle
 from neo4j import GraphDatabase
 
 # Load English tokenizer, tagger, parser, NER and word vectors
@@ -44,7 +45,9 @@ def create_dict(text, rootText, type, index, count):
                  'count': count,
                  'dbpediaURL': "",
                  'entityOfType': "",
-                 'rdfURL': ""})
+                 'rdfURL': "",
+                 'rdfTypes': "",
+                 'rdfTypesURL': ""})
 
 
 def get_dbpedia_url_of_term(term):
@@ -71,83 +74,125 @@ def get_rdf_xml_url_of_term(term):
     return url
 
 
+def find_dbo_types_of_term(term):
+    resp = requests.get(get_rdf_xml_url_of_term(term))
+    resp.raise_for_status()
+    soupSource = BeautifulSoup(resp.text, "html.parser")
+    results = soupSource.find_all("rdf:type")
+    rdfTypesURL = []
+    for result in results:
+        # if "http://dbpedia.org/ontology/" in str(result):
+        #     print(result['rdf:resource'])
+        #     print(str(result['rdf:resource']).split("/")[-1])
+        # if "owl#Thing" in str(result):
+        #     print(result['rdf:resource'])
+        #     print(str(result['rdf:resource']).split("#")[-1])
+        #     print(result['rdf:resource'])
+        if "http://dbpedia.org/ontology/" in str(result) or "owl#Thing" in str(result):
+            rdfTypesURL.append(result['rdf:resource'])
+
+    return rdfTypesURL
+
+
+def parse_dbo_types_from_url(urlList):
+    rdfTypes = []
+    for url in urlList:
+        if "http://dbpedia.org/ontology/" in str(url):
+            rdfTypes.append(str(url).split("/")[-1])
+
+        elif "owl#Thing" in str(url):
+            rdfTypes.append(str(url).split("#")[-1])
+
+    return rdfTypes
+
+
+def parse_one_dbo_type_from_url(url):
+    if "http://dbpedia.org/ontology/" in str(url):
+        return str(url).split("/")[-1]
+
+    elif "owl#Thing" in str(url):
+        return str(url).split("#")[-1]
+
+
+############################################################################
+########                       Reading Text                         ########
+############################################################################
+
 # Example scenario text is taken from "Scientific American: Feature Article: The Semantic Web: May 2001
 # by TIM BERNERS-LEE, JAMES HENDLER and ORA LASSILA"
-text = ("""
-The entertainment system was belting out the Beatles' "We Can Work It Out" when the phone rang. When Pete
-answered, his phone turned the sound down by sending a message to all the other local devices that had a volume control.
-His sister, Lucy, was on the line from the doctor's office: "Mom needs to see a specialist and then has to have a series
-of physical therapy sessions. Biweekly or something. I'm going to have my agent set up the appointments." Pete
-immediately agreed to share the chauffeuring. At the doctor's office, Lucy instructed her Semantic Web agent through her
-handheld Web browser. The agent promptly retrieved information about Mom's prescribed treatment from the doctor's agent,
-looked up several lists of providers, and checked for the ones in-plan for Mom's insurance within a 20-mile radius of
-her home and with a rating of excellent or very good on trusted rating services. It then began trying to find a match
-between available appointment times (supplied by the agents of individual providers through their Web sites) and Pete's
-and Lucy's busy schedules. (The emphasized keywords indicate terms whose semantics, or meaning, were defined for the
-agent through the Semantic Web.)
-In a few minutes the agent presented them with a plan. Pete didn't like it—University Hospital was all the way across
-town from Mom's place, and he'd be driving back in the middle of rush hour. He set his own agent to redo the search
-with stricter preferences about location and time. Lucy's agent, having complete trust in Pete's agent in the context of
-the present task, automatically assisted by supplying access certificates and shortcuts to the data it had already
-sorted through.
-Almost instantly the new plan was presented: a much closer clinic and earlier times—but there were two
-warning notes. First, Pete would have to reschedule a couple of his less important appointments. He checked what they
-were—not a problem. The other was something about the insurance company's list failing to include this provider under
-physical therapists: "Service type and insurance plan status securely verified by other means," the agent reassured him.
-"(Details?)"
-Lucy registered her assent at about the same moment Pete was muttering, "Spare me the details," and it was all set.
-(Of course, Pete couldn't resist the details and later that night had his agent explain how it had found that provider
-even though it wasn't on the proper list.)
-""")
-
-doc = nlp(text)
+f = open("input-text.txt", "r")
+text = f.read()
 
 termList = []
+# flag to load list from the stored file or not
+loadListFromFile = True
 
-# parse terms
-print("Parsing terms...")
-# entities
-for entity in doc.ents:
-    if entity.root.pos_ != "NUM" and entity.root.pos_ != "ADV" and entity.root.pos_ != "NOUN":
-        myTerm = entity.text.replace("\n", " ").title()
-        isMatched = False
+if loadListFromFile:
+    # loading list from the stored file
+    with open("list-store.txt", 'rb') as f:
+        termList = pickle.load(f)
 
-        for index, dict_item in enumerate(termList):
-            if myTerm == dict_item['term']:
-                termList[index]['count'] += 1
-                isMatched = True
+else:
 
-        if not isMatched:
-            termList.append(create_dict(entity.text.replace("\n", " "), myTerm, entity.root.pos_, entity.root.i, 1))
+    ############################################################################
+    ########                       Parsing Terms                        ########
+    ############################################################################
 
-# noun chunks
-for noun in doc.noun_chunks:
+    doc = nlp(text)
 
-    if noun.root.pos_ != "PRON":
-        myTerm = noun.root.lemma_.title()
-        isMatched = False
+    # parse terms
+    print("Parsing terms...")
+    # entities
+    for entity in doc.ents:
+        if entity.root.pos_ != "NUM" and entity.root.pos_ != "ADV" and entity.root.pos_ != "NOUN":
+            myTerm = entity.text.replace("\n", " ").title()
+            isMatched = False
 
-        for index, dict_item in enumerate(termList):
-            if myTerm == dict_item['term']:
-                termList[index]['count'] += 1
-                isMatched = True
+            for index, dict_item in enumerate(termList):
+                if myTerm == dict_item['term']:
+                    termList[index]['count'] += 1
+                    isMatched = True
 
-        if not isMatched:
-            termList.append(create_dict(noun.text.replace("\n", " "), myTerm, noun.root.pos_, noun.root.i, 1))
-print("Parsing terms...DONE")
+            if not isMatched:
+                termList.append(create_dict(entity.text.replace("\n", " "), myTerm, entity.root.pos_, entity.root.i, 1))
 
+    # noun chunks
+    for noun in doc.noun_chunks:
 
-# find entity of type of terms
-print("Finding entity of type & RDF/XML URL of terms...")
-for dict_item in termList:
-    try:
-        dict_item['entityOfType'] = get_entity_of_type(dict_item['term'])
-        dict_item['dbpediaURL'] = get_dbpedia_url_of_term(dict_item['term'])
-        dict_item['rdfURL'] = get_rdf_xml_url_of_term(dict_item['term'])
-    except requests.exceptions.HTTPError:
-        print("Term = ", dict_item['term'], " does not have an entity of type!")
-print("Finding entity of type of terms...DONE")
+        if noun.root.pos_ != "PRON":
+            myTerm = noun.root.lemma_.title()
+            isMatched = False
 
+            for index, dict_item in enumerate(termList):
+                if myTerm == dict_item['term']:
+                    termList[index]['count'] += 1
+                    isMatched = True
+
+            if not isMatched:
+                termList.append(create_dict(noun.text.replace("\n", " "), myTerm, noun.root.pos_, noun.root.i, 1))
+    print("Parsing terms...DONE")
+
+    ############################################################################
+    ########                     Creating List                          ########
+    ############################################################################
+
+    # find entity of type of terms
+    print("Finding entity of type & RDF/XML URL of terms...")
+    for dict_item in termList:
+        try:
+            dict_item['entityOfType'] = get_entity_of_type(dict_item['term'])
+            dict_item['dbpediaURL'] = get_dbpedia_url_of_term(dict_item['term'])
+            dict_item['rdfURL'] = get_rdf_xml_url_of_term(dict_item['term'])
+            dict_item['rdfTypesURL'] = find_dbo_types_of_term(dict_item['term'])
+            dict_item['rdfTypes'] = parse_dbo_types_from_url(dict_item['rdfTypesURL'])
+        except requests.exceptions.HTTPError:
+            print("Term = ", dict_item['term'], " does not have an entity of type!")
+
+    print("Finding entity of type of terms...DONE")
+
+    # storing list to a file
+    with open("list-store.txt", 'wb') as f:
+        pickle.dump(termList, f)
 
 # print all stored data
 print("Printing all the stored data...\n")
@@ -156,19 +201,90 @@ rows = [x.values() for x in termList]
 print(tabulate(rows, header))
 print("\nPrinting all the stored data...DONE\n")
 
+############################################################################
+########                          Neo4j                             ########
+############################################################################
 
 print("Importing RDF data to Neo4j Graph Database...")
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "123456"))
+session = driver.session()
 
 
-def add_rdf(tx, uri):
-    tx.run("""CALL semantics.importRDF("%s","RDF/XML")""" % uri)
+def check_node(name, url):
+    query = """
+        MATCH (a:Node {name: '%s', url: '%s'})
+        RETURN (COUNT(a))
+        """ % (name, url)
+    results = session.run(query)
+
+    for result in results:
+        count = result[0]
+
+    if count == 0:
+        # node does not exist
+        return True
+    else:
+        # node exists
+        return False
 
 
-with driver.session() as session:
-    for dict_item in termList:
-        if dict_item['rdfURL'] != "":
-            session.write_transaction(add_rdf, dict_item['rdfURL'])
+def create_node(name, url):
+    if check_node(name, url):
+        query = """
+                MERGE (a:Node {name: '%s', url: '%s'})
+                """ % (name, url)
+        results = session.run(query)
+        return True
+    else:
+        print("Node is already existent")
+        return False
+
+
+def create_relationship(fromName, fromUrl, toName, toUrl):
+    query = """
+            MATCH (a:Node), (b:Node)
+            WHERE a.name='%s' AND a.url='%s' AND b.name='%s' AND b.url='%s'
+            MERGE  (a)-[:TYPE_OF]->(b)
+            """ % (fromName, fromUrl, toName, toUrl)
+    results = session.run(query)
+
+
+for term in termList:
+    # if rdf type list is not empty
+    if term['rdfTypes']:
+        fromName = term['term']
+        fromUrl = term['rdfURL']
+        # create term node if not existent and continue
+        if create_node(fromName, fromUrl):
+            for rdfType in term['rdfTypesURL']:
+                toName = parse_one_dbo_type_from_url(rdfType)
+                toUrl = rdfType
+                # create entity type node if not existent
+                create_node(toName, toUrl)
+                # create relationship between term and type
+                create_relationship(fromName, fromUrl, toName, toUrl)
+
 
 driver.close()
 print("Importing RDF data to Neo4j Graph Database...DONE")
+
+
+############################################################################
+########                    Neo4j with NeoSemantic                  ########
+############################################################################
+
+# print("Importing RDF data to Neo4j Graph Database with NeoSemantics...")
+# driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "123456"))
+#
+#
+# def add_rdf(tx, uri):
+#     tx.run("""CALL semantics.importRDF("%s","RDF/XML")""" % uri)
+#
+#
+# with driver.session() as session:
+#     for dict_item in termList:
+#         if dict_item['rdfURL'] != "":
+#             session.write_transaction(add_rdf, dict_item['rdfURL'])
+#
+# driver.close()
+# print("Importing RDF data to Neo4j Graph Database with NeoSemantics...DONE")
